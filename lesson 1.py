af2a4993c5b35e8f8ae2396352e3b76bcfd3a138
Ziya-Meiramova <84172592+Ziya-Meiramova@users.noreply.github.com>
2021-06-17 12:59:01 +0600

    Код, который писали на первом уроке

---
+++ lesson 1.py
@@ -0,0 +1,107 @@
+age = 25 #переменная с типом int
+gpa = 3.6
+name = " Ziya"
+# print(age)
+
+surname = "Meiramova    "
+full_name = surname + name
+
+# print(full_name, age, gpa) # этот код работает для вывода в консоль
+# print(type(gpa))
+
+# pravda = False
+# print(type(pravda))
+# print(int(pravda))
+
+a = "5"
+# b = "7"
+# c = a + b #c = "57"
+# print(int(c) - 2)
+# print(int(a) + int(b))
+
+# a = 0
+
+# a = 5
+# print(a)
+
+# a = 2
+# b = 3
+
+# a += b # a = a + b  a = 5
+# a += 5 # a = a + 5  a = 10
+# a -= 2 # a = a - 2  a = 8
+# a *= 2 # a = a * 2  a = 16
+# a /= 4 # a = a / 4  a = 4
+# print(a)
+
+# a = [1, 2, 3, 4]
+# for i in range(len(a)):
+#   print(a[0])
+
+
+# print(c)
+# print(a + b, a - b, a * b, int(a / b))
+# print(a / b, a // b)
+
+# print(a % b)
+# print(a**b, b**a)
+
+
+# name = input("Введите свое имя: ")
+# surname = input("Введите свою фамилию: ")
+# age = int(input("Введите свой возраст: "))
+# print("Отлично!")
+
+# age = age + 5
+# print(age)
+
+# 1 task
+# a = int(input("Введите первый катет: "))
+# b = int(input("Введите второй катет: "))
+
+# с = (a**2 + b**2)**(1/2)
+# print(c)
+
+# task 2
+# print("Здравствуйте!")
+# name = input("Введите свое имя: ")
+# print("Здравствуйте! ", name)
+
+#task 3
+# n = int(input()) #10 пример
+# k = int(input()) # 23
+
+# print(" У каждого ученика столько яблок: ", k // n)
+# print(" В корзине осталось столько яблок: ", k % n)
+
+#task 4
+# x = 109
+# v = int(input()) #20
+# t = int(input()) #10
+# # t = 20
+# # v = x / t
+# # print(v)
+
+# print("остановился на таком км: ", (v*t) % x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
