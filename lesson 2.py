8b67d977d9c6f5b0cf40de7b4b36757e0d62e0c9
Ziya-Meiramova <84172592+Ziya-Meiramova@users.noreply.github.com>
2021-06-19 21:01:23 +0600

    Add files via upload

---
+++ lesson 2.py
@@ -0,0 +1,101 @@
+# a = 8
+# if a == 5:
+#     print("OK")
+# else:
+#     print("TRY AGAIN")
+
+# age = int(input())
+# if age < 18:
+#     print("Нельзя")
+# elif age >= 18 and age < 21:
+#     print("с родственниками")
+# else:
+#     print("можно")
+
+# task 1
+# a = int(input())
+# b = int(input())
+# if a > b:
+#     print(a)
+# elif a < b:
+#     print(b)
+# else:
+#     print("равны")
+
+# year = int(input())
+# if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+#     print("leap year")
+# else:
+#     print("NO")
+
+#and должно с двух сторон условие работать
+#or достаточно одного условия
+
+#task 3
+# x = int(input())
+# # 0
+# if x > 0:
+#     print(1)
+# elif x < 0:
+#     print(-1)
+# else:
+#     print(0)
+
+#task 4
+# a = int(input())
+# b = int(input())
+# c = int(input()) # a = 5 b = 5 c = 2
+# if a >= b and a >= c:
+#     print(a)
+# elif b >= a and b >= c:
+#     print(b)
+# else:
+#     print(c)
+
+#task 5 Ладья
+# x1 = int(input())
+# y1 = int(input())
+# x2 = int(input())
+# y2 = int(input())
+# x3 = 3
+# y3 = 3
+# if x1 == x2 or y1 == y2:
+#     print("OK")
+# else:
+#     print("NOT OK")
+
+# print() вывод в консоль
+# input() ввод через консоль
+# int() меняем типизацию на цифровой тип
+# if если
+# elif дополнительные условия для если
+# else остальные случаи
+# or или, код работает если хотя бы одно условие верно
+# and и, код работает только когда два условия работают
+
+#task 6
+# x1 = int(input()) # 3
+# y1 = int(input()) # 2
+# x2 = int(input()) # 6
+# y2 = int(input()) # 4
+#
+# if x2 - x1 == y2 - y1:
+#     print("OK")
+# else:
+#     print("NOT OK")
+
+#task 7
+# x1 = int(input()) # 3
+# y1 = int(input()) # 2
+# x2 = int(input()) # 6
+# y2 = int(input()) # 4
+#
+# if x1 == x2 or y1 == y2 or x2 - x1 == y2 - y1:
+#     print("OK")
+# else:
+#     print("NOT OK")
+
+
+
+
+
