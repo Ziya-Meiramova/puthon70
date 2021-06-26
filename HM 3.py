78d016453964c3fe7d39ba3c6bc8e161c9c4399d
Ziya-Meiramova <84172592+Ziya-Meiramova@users.noreply.github.com>
2021-06-26 21:01:21 +0600

    Add files via upload

---
+++ HM 3.py
@@ -0,0 +1,29 @@
+# 1
+# x = int(input())
+# p = int(input())
+# y = int(input())
+# i = 0
+#
+# while x < y:
+#     x = x * (1 + p/100)
+#     x = int(100 * x)/100
+#     i = i + 1
+# print(i)
+
+# task 2
+# x = int(input())
+# y = int(input())
+# i = 1
+# while x < y:
+#     x = x * 1.1
+#     i+=1
+# print(i)
+
+#task 3
+# max = 0   #
+# a = -1
+# while a!=0:
+#     a = int(input())
+#     if a > max:
+#         max = a
+# print(max)
